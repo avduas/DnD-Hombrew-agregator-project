f2b443f7acdc972ab35d6868c031d079d69c304a
avduas <tankraketa1995@gmail.com>
2025-05-02 19:46:01 +0300

    .env try

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 
-const API_URL =  process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 // process.env.REACT_APP_API_URL 
 
